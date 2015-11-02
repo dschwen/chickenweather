a72378994c9d26763fbe7dcccd858b8c1502f43a
Daniel Schwen <daniel@schwen.de>
2015-11-02 11:22:05 -0700

    add package file, fetch latest data

--- server/main.js
+++ server/main.js
@@ -1,14 +1,24 @@
-var fs = require('fs');
+var fs = require('fs'),
+    dateFormat = require('dateformat');
 
-path = '/var/log/coop/20151101';
+/**
+ * Get the current logfile name
+ */
+var now = new Date(),
+    date = dateFormat(now, "yyyymmdd"),
+    path = '/var/log/coop/' + date;
 
+/**
+ * Read file
+ */
 fs.readFile(path, function(err, f){
   var array = f.toString().split('\n');
   
   for (i in array) {
-    col = array[i].split(' ');
-    console.log(col);
+    array[i] = array[i].split(' ');
   }
+
+  console.log(JSON.stringify(array));
 });
 
 
