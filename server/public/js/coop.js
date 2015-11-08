961ff387a6e2f8fd7b4032d82c93e1c4846783ed
Daniel Schwen <daniel@schwen.de>
2015-11-07 22:01:47 -0700

    external values

--- server/public/js/coop.js
+++ server/public/js/coop.js
@@ -3,15 +3,17 @@ $(function() {
   $.getJSON('/data/today.json', function(data) {
     var temperature = [], Tmin = null, Tmax = null,
         humidity = [], Hmin = null, Hmax = null,
-        exttemp = [],
-        exthumi = [];
+        exttemp = [], eTmin = null, eTmax = null,
+        exthumi = [], eHmin = null, eHmax = null;
 
     // extract plot data
     for (var i = 0; i < data.length; ++i) {
       // milli seconds
       var time = (data[i][0]*60 + data[i][1]) * 60 * 1e3,
           T = data[i][2],
-          H = data[i][3];
+          H = data[i][3],
+          eT = data[i][4],
+          eH = data[i][5];
 
       temperature.push([time, T]);
       humidity.push([time, H]);
@@ -21,8 +23,13 @@ $(function() {
       if (Hmin === null || H < Hmin) Hmin = H;
       if (Hmax === null || H > Hmax) Hmax = H;
 
-      exttemp.push([time, data[i][4]]);
-      exthumi.push([time, data[i][5]]);
+      exttemp.push([time, eT]);
+      exthumi.push([time, eH]);
+
+      if (eTmin === null || eT < eTmin) eTmin = eT;
+      if (eTmax === null || eT > eTmax) eTmax = eT;
+      if (eHmin === null || eH < eHmin) eHmin = eH;
+      if (eHmax === null || eH > eHmax) eHmax = eH;
     }
 
     // current values
@@ -34,7 +41,15 @@ $(function() {
       $('#Hnow').text(data[last][3]);
       $('#Hmin').text(Hmin);
       $('#Hmax').text(Hmax);
-      $('#values').fadeIn();
+
+      $('#eTnow').text(data[last][4]);
+      $('#eTmin').text(eTmin);
+      $('#eTmax').text(eTmax);
+      $('#eHnow').text(data[last][5]);
+      $('#eHmin').text(eHmin);
+      $('#eHmax').text(eHmax);
+
+      $('.values').fadeIn();
     }
  
     $.plot("#plot", [
